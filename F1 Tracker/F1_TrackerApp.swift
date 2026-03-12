a7392390cc437bc134481c681dab0ed80c964b52
Pushkar Aagnihotri <agnihotripush@gmail.com>
2026-03-11 23:22:28 -0700

    Initial Commit

---
+++ F1 Tracker/F1_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1_TrackerApp.swift
+//  F1 Tracker
+//
+//  Created by csuftitan on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct F1_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
